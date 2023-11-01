14640c3cc5567ad8a5fcd3e23706b4bc45f98b68
Joseph Luca San Juan Thompson <91873591+JosephLucaSanJuan@users.noreply.github.com>
2023-11-01 21:41:32 +0100

    Add files via upload

---
+++ UD2T1SanJuanThompsonJosephLuca/UD2T1SanJuanThompsonJosephLuca/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD2T1SanJuanThompsonJosephLuca
+{
+    internal class Empleado
+    {
+        private string nombre;
+        private string apellido1;
+        private string apellido2;
+        private int edad;
+        private string NIF;
+        private double salario;
+    }
+}
---
+++ UD2T1SanJuanThompsonJosephLuca/UD2T1SanJuanThompsonJosephLuca/Program.cs
@@ -0,0 +1,27 @@
+﻿///////////////////////////////////////////
+// Tarea: UD2T1
+// Alumno/a: David Quest
+// Curso: 2023/2024
+///////////////////////////////////////////
+
+namespace UD2T1SanJuanThompsonJosephLuca
+{
+    internal class UD2_Tarea1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Introduce el nombre del empleado:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Introduce su primer apellido");
+            string apellido1 = Console.ReadLine();
+            Console.WriteLine("Introduce su seguindo apellido");
+            string apellido2 = Console.ReadLine();
+            Console.WriteLine("Introduce la edaddel empleado: ");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el NIF:");
+            string NIF = Console.ReadLine();
+        }
+    }
+}

52b56bb41da76dc3d464e09dad93687f2221cfce
Joseph Luca San Juan Thompson <91873591+JosephLucaSanJuan@users.noreply.github.com>
2023-11-07 13:03:29 +0100

    Add files via upload

--- UD2T1SanJuanThompsonJosephLuca/UD2T1SanJuanThompsonJosephLuca/Empleado.cs
+++ UD2T1SanJuanThompsonJosephLuca/UD2T1SanJuanThompsonJosephLuca/Empleado.cs
@@ -14,5 +14,85 @@ namespace UD2T1SanJuanThompsonJosephLuca
         private int edad;
         private string NIF;
         private double salario;
+
+        public Empleado(string nombre, string apellido1, string apellido2, int edad, string NIF, double salario)
+        {
+            Nombre = nombre;
+            Apellido1 = apellido1;
+            Apellido2 = apellido2;
+            Edad = edad;
+            Nif = NIF;
+            Salario = salario;
+        }
+
+        public string Nombre
+        {
+            get {return nombre; } set { nombre = value; }
+        }
+
+        public string Apellido1
+        {
+            get { return apellido1; }
+            set { apellido1 = value; }
+        }
+
+        public string Apellido2
+        { 
+            get { return apellido2; } set { apellido2 = value; } 
+        }
+
+        public int Edad
+        {
+            get { return edad; } set {  edad = value; }
+        }
+
+        public string Nif
+        {
+            get { return NIF; } set {  NIF = value; }
+        }
+
+        public double Salario
+        {
+            get { return salario; } set { salario = value; }
+        }
+
+        public string toString()
+        {
+            String sb = "Nombre: " + nombre+" "+apellido1+" "+apellido2;
+            sb += "\nEdad: " +edad+" años";
+            sb += "\nNIF: " + NIF;
+            sb += "\nSalario " +salario+"€";
+            return sb;
+        }
+
+        public void MostrarNombre()
+        {
+            Console.WriteLine(NombreCompleto());
+        }
+
+        public void MostrarNif()
+        {
+            Console.WriteLine(Nif);
+        }
+
+        public void ActualizarSalario(double sal)
+        {
+            Salario = sal;
+        }
+
+        public void ActualizarNif(string nif)
+        {
+            Nif = nif;
+        }
+
+        public void MostrarEdad()
+        {
+            Console.WriteLine(Edad);
+        }
+
+        public String NombreCompleto()
+        {
+            return nombre + " " + apellido1 + " " + apellido2;
+        }
     }
 }
--- UD2T1SanJuanThompsonJosephLuca/UD2T1SanJuanThompsonJosephLuca/Program.cs
+++ UD2T1SanJuanThompsonJosephLuca/UD2T1SanJuanThompsonJosephLuca/Program.cs
@@ -12,16 +12,86 @@ namespace UD2T1SanJuanThompsonJosephLuca
         {
             Console.WriteLine("Hello, World!");
 
+            // Introducimos los datos del empleado
+            // Nombre
             Console.WriteLine("Introduce el nombre del empleado:");
             string nombre = Console.ReadLine();
+            // Apellidos
             Console.WriteLine("Introduce su primer apellido");
             string apellido1 = Console.ReadLine();
-            Console.WriteLine("Introduce su seguindo apellido");
+            Console.WriteLine("Introduce su segundo apellido");
             string apellido2 = Console.ReadLine();
-            Console.WriteLine("Introduce la edaddel empleado: ");
+            // Edad
+            Console.WriteLine("Introduce la edad del empleado");
             int edad = int.Parse(Console.ReadLine());
-            Console.WriteLine("Introduce el NIF:");
-            string NIF = Console.ReadLine();
+            // NIF
+            Console.WriteLine("Introduce el NIF");
+            string nif = Console.ReadLine();
+            // Salario
+            Console.WriteLine("Introduce el salario");
+            double salario = double.Parse(Console.ReadLine());
+
+            // Creamos el empleado
+            Empleado[] empleados = new Empleado[100];
+            Empleado empleado = new Empleado(nombre, apellido1, apellido2, edad, nif, salario);
+            Boolean correcto = true;
+
+
+            while (correcto) { 
+                Console.WriteLine("\n1. Salir");
+                Console.WriteLine("2. Actualiza el salario");
+                Console.WriteLine("3. Muestra el nombre de un empleado");
+                Console.WriteLine("4. Muestra el NIF");
+                Console.WriteLine("5. Actualiza el NIF");
+                Console.WriteLine("6. Muestra la edad de un empleado");
+                Console.WriteLine("7. Muestra los datos de un empleado");
+                int opcion = int.Parse(Console.ReadLine());
+                switch (opcion) {
+                    case 1:
+                        correcto = false;
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Introduce el nuevo salario:");
+                        double newSalario = double.Parse(Console.ReadLine());
+                        empleado.ActualizarSalario(newSalario);
+                        correcto = true;
+                        break;
+
+                    case 3:
+                        //Console.WriteLine("Introduce el empleado cuyo nombre deseas mostrar: ");
+                        empleado.MostrarNombre();
+                        correcto = true;
+                        break;
+
+                     case 4:
+                        //Console.WriteLine("Introduce el empleado cuyo NIF quieres mostrar: ");
+                        empleado.MostrarNif();
+                        correcto = true;
+                        break;
+
+                     case 5:
+                        Console.WriteLine("Introduce el nuevo NIF:");
+                        string newNif = Console.ReadLine();
+                        empleado.ActualizarNif(newNif);
+                        correcto = true;
+                        break;
+
+                     case 6:
+                        //Console.WriteLine("Introduce el empleado cuya edad quieres mostrar: ");
+                        empleado.MostrarEdad();
+                        correcto = true;
+                        break;
+
+                    case 7:
+                        //Console.WriteLine("Introduce el empleado cuyos datos quieres mostrar: ");
+                        Console.WriteLine(empleado.toString());
+                        correcto = true;
+                        break;
+                }
+            }
+
+            
         }
     }
 }
